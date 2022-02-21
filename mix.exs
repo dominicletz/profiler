1a8987cad683b85a4fd35b525d629fcc49af297a
Dominic Letz <dominic@diode.io>
2022-02-21 19:08:52 +0100

    hex release 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Profiler.MixProject do
   def project do
     [
       app: :profiler,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
