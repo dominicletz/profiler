8f03ee0438fa0fce13293535ba8a39ea5d0cf0fb
Dominic Letz <dominic.letz@berlin.de>
2025-07-08 19:07:46 +0200

    v0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Profiler.MixProject do
   def project do
     [
       app: :profiler,
-      version: "0.3.3",
+      version: "0.4.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
